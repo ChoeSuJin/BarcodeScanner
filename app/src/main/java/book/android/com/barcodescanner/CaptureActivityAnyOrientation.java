e95a515bb7377c1ab8b8196503a2ac88f0f36633
SujinChoe <green41255@nate.com>
2017-05-14 03:50:31 +0900

    Barcode CaptureActivityAnyOrientation 생성 AndroidManifest.xml 추가 및 수정

---
+++ app/src/main/java/book/android/com/barcodescanner/CaptureActivityAnyOrientation.java
@@ -0,0 +1,12 @@
+package book.android.com.barcodescanner;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.journeyapps.barcodescanner.CaptureActivity;
+
+/**
+ * Created by SUJIN CHOE on 2017-05-14.
+ */
+
+public class CaptureActivityAnyOrientation extends CaptureActivity {
+
+}
--- app/src/main/java/book/android/com/barcodescanner/MainActivity.java
+++ app/src/main/java/book/android/com/barcodescanner/MainActivity.java
@@ -1,13 +1,15 @@
 package book.android.com.barcodescanner;
 
+import android.app.Activity;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.zxing.integration.android.IntentIntegrator;
+
 public class MainActivity extends AppCompatActivity {
-
-
 
 
     @Override
@@ -16,12 +18,29 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Button btnCamera;
         btnCamera = (Button) findViewById(R.id.btnOpenCamera);
+        /*IntentIntegrator integrator = new IntentIntegrator(this);
+        integrator.setCaptureActivity(CaptureActivityAnyOrientation.class);
+        integrator.setOrientationLocked(true);
+        integrator.initiateScan();*/
 
         btnCamera.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
+                Activity activity = new MainActivity();
+
+                IntentIntegrator integrator = new IntentIntegrator(activity);
+                integrator.setCaptureActivity(CaptureActivityAnyOrientation.class);
+                integrator.setOrientationLocked(true);
+                integrator.setPrompt("바코드를 스캔합니다.");
+                integrator.initiateScan();
+
             }
         });
+
+
+
+
+
     }
 }
