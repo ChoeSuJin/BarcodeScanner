a03089922e1fdaf69e84d976482a8e375aefa5f8
SujinChoe <green41255@nate.com>
2017-05-13 23:57:55 +0900

    Barcode 리더를 위한 lib 추가

--- app/src/main/java/book/android/com/barcodescanner/MainActivity.java
+++ app/src/main/java/book/android/com/barcodescanner/MainActivity.java
@@ -2,12 +2,26 @@ package book.android.com.barcodescanner;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btnCamera;
+        btnCamera = (Button) findViewById(R.id.btnOpenCamera);
+
+        btnCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
